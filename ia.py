0e5fcf012822f1411ad7fec987d98a1314391115
debora cordova <deboracordovak@gmail.com>
2026-01-29 21:53:49 -0300

    Renomeia assistente para Deia e atualiza textos

--- ia.py
+++ ia.py
@@ -9,7 +9,8 @@ def responder(mensagem):
     completion = client.chat.completions.create(
         model="llama-3.1-8b-instant",
         messages=[
-            {"role": "system", "content": "Você é a assistente DehAI, educada, útil, direta e engraçada."},
+            {"role": "system", "content": "Você é a assistente Deia, educada, útil, direta, prestativa, corporativa, engraçada e acolhedora"
+             "responda sempre em português do Brasil, com clareza e objetividade."},
             {"role": "user", "content": mensagem}
         ]
     )
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from ia import responder
 
-print(" DehAI iniciada! Digite 'sair' para encerrar.\n")
+ASSISTENTE_NOME = "Deia" 
+
+print(" Deia iniciada! Digite 'sair' para encerrar.\n")
 
 while True:
     user_input = input("Você: ")
     
     if user_input.lower() == "sair":
-        print("DehAI: Até mais, debora!")
+        print(f"{ASSISTENTE_NOME}:Até logo! Senhorita Debora, estarei sempre a sua disposição.")
         break
 
     resposta = responder(user_input)
-    print(f"DehAI: {resposta}\n")
+    print(f"{ASSISTENTE_NOME}:{resposta}\n")
